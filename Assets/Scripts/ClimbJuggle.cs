8edfe73de52a6fd28f5f093d247e2024480c77a8
Kevin Bryan <kevincbryan@gmail.com>
2020-02-11 17:51:52 -0800

    Tried to get Radius Matching Working

--- Assets/Scripts/ClimbJuggle.cs
+++ Assets/Scripts/ClimbJuggle.cs
@@ -8,20 +8,42 @@ public class ClimbJuggle : MonoBehaviour
     float radius;
     public ThirdPersonCharacterController pCon;
     public GameObject climbable;
-    //public CapsuleCollider climbCap;
+    private CapsuleCollider climbCap;
     
     // Start is called before the first frame update
     void Start()
     {
         pCon = GetComponent<ThirdPersonCharacterController>();
         speed = pCon.speed;
+        climbCap = climbable.GetComponent<CapsuleCollider>();
+        radius = climbCap.radius;
         //radius = climbCap.radius;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 movement = new Vector3(0, 0, 0);
+        MoveToRadius();
+        HorizontalClimb();
+        
+    }
+
+    void MoveToRadius ()
+    {
+        Vector2 dir = new Vector2 (climbable.transform.position.x, climbable.transform.position.z) - new Vector2 (gameObject.transform.position.x, gameObject.transform.position.z);
+        Vector3 endPos;
+        if (dir.magnitude > radius)
+        {
+            endPos = gameObject.transform.position + new Vector3 (dir.x, 0, dir.y).normalized * radius;
+            transform.position = Vector3.MoveTowards(transform.position, endPos, speed * .5f * Time.deltaTime);
+        }
+
+
+    }
+
+    void HorizontalClimb ()
+    {
         transform.RotateAround(climbable.transform.position, Vector3.up, Time.deltaTime * speed * Input.GetAxis("Horizontal"));
     }
+   
 }
